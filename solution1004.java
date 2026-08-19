fc3782e6afec003919929545c35f08896e90479f
tilak <ntilak321@gmail.com>
2026-08-19 22:25:33 +0530

    1004. Max Consecutive Ones III'

---
+++ solution1004.java
@@ -0,0 +1,49 @@
+
+
+import java.util.*;
+
+public class solution1004 {
+    public int findingonce(int [] nums,int k){
+        int l=0;
+        int r=0;
+        int len=0;
+        int zeros=0;
+        int maxlen=0;
+        while(r<nums.length){
+            if(nums[r]==0){
+                zeros++;
+            } 
+            while (zeros>k) {
+                if(nums[l]==0){
+                    zeros--;
+                    
+                }
+                l++;
+            }  
+                    len=r-l+1;
+                    maxlen=Math.max(len, maxlen);
+                r++;
+            
+ 
+    }
+    return maxlen;
+}
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter array length");
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    System.out.println("enter array elements");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    System.out.println("enter key elements");
+    int k=sc.nextInt();
+    solution1004 sn=new solution1004();
+    System.out.println("solution is"+sn.findingonce(arr, k));
+    sc.close();
+}
+}
+
+
