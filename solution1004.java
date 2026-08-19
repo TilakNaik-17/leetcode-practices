1c9842fd61356508f284426528ca64b60db0454f
tilak <ntilak321@gmail.com>
2026-08-19 23:02:22 +0530

    1004. Max Consecutive Ones III optimized

--- solution1004.java
+++ solution1004.java
@@ -13,18 +13,19 @@ public class solution1004 {
             if(nums[r]==0){
                 zeros++;
             } 
-            while (zeros>k) {
+            if (zeros>k) {
                 if(nums[l]==0){
                     zeros--;
-                    
+
                 }
                 l++;
             }  
+            if(zeros<=k){
                     len=r-l+1;
                     maxlen=Math.max(len, maxlen);
-                r++;
-            
- 
+                
+            }
+ r++;
     }
     return maxlen;
 }
